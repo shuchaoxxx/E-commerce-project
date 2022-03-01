eb4fbf7fffeb146ad7740f426e075b72e47522bd
地皮流氓 <1598052467@qq.com>
2022-03-01 21:49:39 +0800

    面包屑组件的封装完成（render、h函数）

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -1,6 +1,8 @@
 import XtxSkeleton from './xtx-skeleton.vue'
 import XtxCarousel from './xtx-carousel.vue'
 import XtxMore from './xtx-more.vue'
+import XtxBread from './xtx-bread.vue'
+import XtxBreadItem from './xtx-bread-item'
 
 import defaultImg from '@/assets/images/200.png'
 
@@ -11,6 +13,8 @@ export default {
     app.component(XtxSkeleton.name, XtxSkeleton)
     app.component(XtxCarousel.name, XtxCarousel)
     app.component(XtxMore.name, XtxMore)
+    app.component(XtxBread.name, XtxBread)
+    app.component(XtxBreadItem.name, XtxBreadItem)
     defineDirective(app)
   }
 }
--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>TopCategory></div>
+  <div class="container">
+    <XtxBread >
+      <XtxBreadItem to="/" > 首页</XtxBreadItem>
+      <XtxBreadItem to="/category" > 电器 </XtxBreadItem>
+      <XtxBreadItem > 冰箱 </XtxBreadItem>
+    </XtxBread>
+  </div>
 </template>
 
 <script>
